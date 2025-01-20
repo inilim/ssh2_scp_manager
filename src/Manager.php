a97823b63272cf0e9e6d50b3f4a5974d10538728
Dmitry <40324321+inilim@users.noreply.github.com>
2025-01-20 11:57:25 +0300

    init

---
+++ src/Manager.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Ssh2TcpManager;
+
+final class Manager
+{
+    /** @var string */
+    protected $host;
+    /** @var int */
+    protected $port;
+    /** @var string */
+    protected $username;
+    /** @var string */
+    protected $password;
+    /** @var ?array */
+    protected $methods;
+    /** @var ?array */
+    protected $callbacks;
+    /** @var ?resource */
+    protected $connect = null;
+
+    function __construct(
+        string $host,
+        string $username,
+        string $password,
+        int $port         = 22,
+        ?array $methods   = null,
+        ?array $callbacks = null
+    ) {
+        $this->host      = $host;
+        $this->port      = $port;
+        $this->username  = $username;
+        $this->password  = $password;
+        $this->methods   = $methods;
+        $this->callbacks = $callbacks;
+    }
+
+    /**
+     * @return bool
+     */
+    function send(
+        string $localFile,
+        string $remoteFile,
+        int $createMode = 0644
+    ) {
+        $this->init();
+        return \ssh2_scp_send($this->connect, $localFile, $remoteFile, $createMode);
+    }
+
+    /**
+     * @return bool
+     */
+    function get(
+        string $remoteFile,
+        string $localFile
+    ) {
+        $this->init();
+        return \ssh2_scp_recv($this->connect, $remoteFile, $localFile);
+    }
+
+    /**
+     * @return bool
+     */
+    function disconnect()
+    {
+        $status = \ssh2_disconnect($this->connect);
+        $this->connect = null;
+        return $status;
+    }
+
+    function __destruct()
+    {
+        $this->disconnect();
+    }
+
+    // ------------------------------------------------------------------
+    // 
+    // ------------------------------------------------------------------
+
+    /**
+     * @return void
+     */
+    protected function connect()
+    {
+        $resource = \ssh2_connect($this->host, $this->port, $this->methods, $this->callbacks);
+        if ($resource === false) {
+            throw new \Exception('Connection failed');
+        }
+        $this->connect = $resource;
+    }
+
+    /**
+     * @return void
+     */
+    protected function auth()
+    {
+        $status = \ssh2_auth_password($this->connect, $this->username, $this->password);
+        if (!$status) {
+            throw new \Exception('Authentication failed');
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function init()
+    {
+        if ($this->connect) return;
+        $this->connect();
+        $this->auth();
+    }
+}
